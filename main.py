ff3f1e24809c5314b994fc02f246ac25eff24ee1
Sridar Dhandapani <sridar@gmail.com>
2021-05-03 15:26:26 +0100

    Fix tqdm unit scaling

--- main.py
+++ main.py
@@ -158,10 +158,12 @@ def download_book(filename, url):
         else:
             total = int(total)
             # TODO: read more about tqdm
-            for chunk in tqdm(r.iter_content(chunk_size=1024), total=math.ceil(total//1024), unit='KB', unit_scale=True):
-                if chunk:  # filter out keep-alive new chunks
-                    f.write(chunk)
-                    f.flush()
+            with tqdm(total=total, unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+                        f.flush()
+                        pbar.update(len(chunk))
             tqdm.write('Finished ' + filename)
 
 
